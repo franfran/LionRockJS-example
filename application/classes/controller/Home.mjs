cf13858da60386ab7365cd081d67e8b8975ae833
Colin Leung <colin@cowise.co>
2024-04-27 05:43:45 +0800

    chore: add ControllerHome

---
+++ application/classes/controller/Home.mjs
@@ -0,0 +1,23 @@
+import {Controller} from '@lionrockjs/mvc';
+import {ControllerMixinView, Central} from '@lionrockjs/central';
+import fs from 'node:fs';
+
+
+export default class ControllerHome extends Controller{
+  static mixins = [ControllerMixinView];
+
+  async action_index() {
+    const request = this.state.get(Controller.STATE_REQUEST);
+    ControllerMixinView.setTemplate(this.state, 'templates/page', {
+      ipcountry: request.headers['cf-ipcountry'] || 'HK'
+    });
+  }
+
+  async action_page(){
+    const data = fs.readFileSync(Central.VIEW_PATH + '/templates/page.json', 'utf8');
+
+    ControllerMixinView.setTemplate(this.state, 'templates/home', {
+      data
+    });
+  }
+}
