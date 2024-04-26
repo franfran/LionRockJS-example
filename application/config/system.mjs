9b12f2b3da6a495c99102339bcd1ae5fae7c3491
Colin Leung <colin@cowise.co>
2024-04-27 05:19:30 +0800

    chore: remove optional folders

--- application/config/system.mjs
+++
@@ -1,5 +0,0 @@
-import {Central} from '@lionrockjs/central';
-
-export default {
-  debug: (Central.ENV !== Central.ENV_PROD),
-}
--- application/routes.mjs
+++ application/routes.mjs
@@ -1,12 +1,4 @@
-import { RouteList, Central } from '@lionrockjs/central';
-import path from "path";
-import fs from "fs";
+import { RouteList } from '@lionrockjs/central';
 
 RouteList.add('/', 'controller/Home');
 RouteList.add('/pages/:slug', 'controller/Home', 'page');
-
-[...Central.nodePackages.values()].forEach( async x => {
-  const filePath = path.normalize(`${x}/routes.mjs`);
-  if (!fs.existsSync(filePath)) return;
-  await import(filePath);
-});
