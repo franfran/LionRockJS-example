81611923fbfc5c2db0be196573b3b299ab537832
Colin Leung <colin@cowise.co>
2024-04-30 02:49:38 +0800

    chore: fix import view

--- application/import.mjs
+++ application/import.mjs
@@ -1,6 +1,6 @@
 import { Central, ControllerMixinDatabase, ORM } from '@lionrockjs/central';
 import { View } from '@lionrockjs/mvc';
-import AdapterViewLiquid, { LiquidView } from '@lionrockjs/adapter-view-liquid';
+import AdapterViewLiquid, { LiquidView } from '@lionrockjs/adapter-view-liquidjs';
 View.DefaultViewClass = LiquidView;
 
 import { ORMAdapterSQLite, DatabaseAdapterBetterSQLite3 } from "@lionrockjs/adapter-database-better-sqlite3";
