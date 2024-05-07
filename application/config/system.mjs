ac62dde8f582a9e4c2ffafc07fbb4cec949722f4
Colin Leung <colin@cowise.co>
2024-05-07 23:59:24 +0800

    chore: disable serve static

--- application/config/system.mjs
+++ application/config/system.mjs
@@ -2,5 +2,5 @@ import {Central} from '@lionrockjs/central';
 
 export default {
   debug: (Central.ENV !== Central.ENV_PROD),
-  serve_static_file: Central.ENV === Central.ENV_DEVE,
+  serve_static_file: false,
 }
