4c41ab5c95cfee8018945bc95c88954cd7de033a
Colin Leung <colin@cowise.co>
2024-05-08 00:59:53 +0800

    chore: use fastify

--- application/config/site.mjs
+++ application/config/site.mjs
@@ -1,9 +1,9 @@
-import { ServerAdapterNodeHTTP } from "@lionrockjs/platform-web-node-http";
+import { ServerAdapterFastify as ServerAdapter } from "@lionrockjs/platform-web-fastify";
 import { Central } from "@lionrockjs/central";
 
 export default {
   platform:{
-    adapter: ServerAdapterNodeHTTP
+    adapter: ServerAdapter
   },
   databaseFolder: `${Central.APP_PATH}/../database`,
   notFound: '/pages/404',
