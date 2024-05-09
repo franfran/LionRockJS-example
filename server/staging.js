48a9f125cc67a19cef08732228b1627ef941c4eb
Colin Leung <colin@cowise.co>
2024-05-09 14:55:55 +0800

    update port

--- server/staging.js
+++ server/staging.js
@@ -5,7 +5,7 @@ import Server from './Server.mjs';
 Central.ENV = Central.ENV_STAG;
 
 (async () => {
-  const s = new Server(parseInt(process.env.PORT ?? 8000) + 2);
+  const s = new Server(parseInt(process.env.PORT ?? '8000') + 5);
   await s.setup();
   await s.listen();
 })();
