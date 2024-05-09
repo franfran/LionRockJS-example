6ba21a2eabb23fe8b1156a5d89437650268d9981
Colin Leung <colin@cowise.co>
2024-05-09 14:49:13 +0800

    update ports

--- server/production.js
+++ server/production.js
@@ -5,7 +5,7 @@ import Server from './Server.mjs';
 Central.ENV = Central.ENV_PROD;
 
 (async () => {
-  const s = new Server(parseInt(process.env.PORT ?? 8000) + 3);
+  const s = new Server(parseInt(process.env.PORT ?? '8000') + 6);
   await s.setup();
   await s.listen();
 })();
--- server/testing.js
+++ server/testing.js
@@ -5,7 +5,7 @@ import Server from './Server.mjs';
 Central.ENV = Central.ENV_TEST;
 
 (async () => {
-  const s = new Server(parseInt(process.env.PORT ?? 8000) + 1);
+  const s = new Server(parseInt(process.env.PORT ?? '8000') + 4);
   await s.setup();
   await s.listen();
 })();
