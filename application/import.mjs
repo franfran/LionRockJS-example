eb34926e08ae607f940b501040393f3948b9c5a2
Colin Leung <colin@cowise.co>
2024-05-11 03:19:57 +0800

    fix: Model

--- application/import.mjs
+++ application/import.mjs
@@ -1,10 +1,10 @@
-import { Central, ControllerMixinDatabase, ORM } from '@lionrockjs/central';
+import { Central, ControllerMixinDatabase, Model } from '@lionrockjs/central';
 import { View } from '@lionrockjs/mvc';
 import AdapterViewLiquid, { LiquidView } from '@lionrockjs/adapter-view-liquidjs';
 View.DefaultViewClass = LiquidView;
 
 import { ORMAdapterSQLite, DatabaseAdapterBetterSQLite3 } from "@lionrockjs/adapter-database-better-sqlite3";
-ORM.defaultAdapter = ORMAdapterSQLite;
+Model.defaultAdapter = ORMAdapterSQLite;
 ControllerMixinDatabase.defaultAdapter = DatabaseAdapterBetterSQLite3;
 
 await (async () => {
